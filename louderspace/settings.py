ee6ec6a1fc14cab5eea52808b37f58e0d1d1ee6f
Philip B Tyler <philip@theblacktux.com>
2015-08-16 23:21:20 -0700

    Basic basic basic page. but good django stuff going on

--- louderspace/settings.py
+++ louderspace/settings.py
@@ -103,5 +103,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_URL = "/static/"
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+)
